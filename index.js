958d7a034173b749a006180eac55ae32c9fc19f2
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-19 17:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Enter the code to remove the main node element under this comment */
 document.querySelector("main").remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement ("")
 document.querySelector("h1#victory").appendChild("Anthimos is the champion")
